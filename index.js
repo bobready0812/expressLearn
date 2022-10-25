6847bd1df2166ac3e39b268aabab69bfecfa8e74
DaehyeonKim <Daehyeonoff@gmail.com>
2022-10-25 21:43:03 +0900

    20221025

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/Express",{useNewUrlParser:true});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("Connected to DB");
+
+db.on("error", (error) => console.log("DB Error", error));
+db.once("open", handleOpen);
 
 app.get('/',(req,res) => res.send("Hello world!"));
 
