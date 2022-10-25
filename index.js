71acd962a7dfb25bc41cf917616b840126091c70
DaehyeonKim <Daehyeonoff@gmail.com>
2022-10-25 21:29:27 +0900

    20221025

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const port = 5000;
 
 app.get('/',(req,res) => res.send("Hello world!"));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}`));
+app.listen(port, () => console.log(`Example app listening on port ${port}`));  
