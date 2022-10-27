2883ed22154b15f98b0e0d71398eac51976029d4
DaehyeonKim <Daehyeonoff@gmail.com>
2022-10-27 21:28:25 +0900

    20221027

--- index.js
+++ index.js
@@ -3,16 +3,33 @@ const app = express();
 const mongoose = require('mongoose');
 const url = 'mongodb+srv://sidae9988:tldnjs99^^@cluster1.9kefxyc.mongodb.net/?retryWrites=true&w=majority'
 const User = require('./models/User');
+const bodyParser = require('body-parser')
 
 mongoose.connect(url).then(() => console.log("DB connected"));
 
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
 const handleHome = async(req,res) => {
-   const users = await User.find({}); //
+   const users = await User.find({}); 
+   console.log(users);                              
    return res.render("home", {users});
 };
 
+const postJoin = async(req,res) => {
+    const {email , name , password} = req.body;
+    await User.create({
+        email,        
+        name,
+        password
+    })
+    res.redirect("/");
+}
+
+
 app.get('/', handleHome);
 app.get('/join', (req,res) => {res.render("join")});
+app.post('/join', postJoin);
 app.set("view engine", "pug");
 app.set("views", process.cwd() + '/views');
 
--- models/User.js
+++ models/User.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name: {type: String},
+    email: {type: String},
+    name: {type:String},
     password: {type: String},
-    username: {type:String},
+    
 })
 
 const User = mongoose.model('User', userSchema);
