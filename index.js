a2313c841c15a2c9b564057b1482a3d76ab158b2
DaehyeonKim <Daehyeonoff@gmail.com>
2022-10-29 19:39:56 +0900

    20221029

--- index.js
+++ index.js
@@ -10,28 +10,7 @@ mongoose.connect(url).then(() => console.log("DB connected"));
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
-const handleHome = async(req,res) => {
-   const users = await User.find({}); 
-   console.log(users);                              
-   return res.render("home", {users});
-};
 
-const postJoin = async(req,res) => {
-    const {email , name , password} = req.body;
-    await User.create({
-        email,        
-        name,
-        password
-    })
-    res.redirect("/");
-}
-
-
-app.get('/', handleHome);
-app.get('/join', (req,res) => {res.render("join")});
-app.post('/join', postJoin);
-app.set("view engine", "pug");
-app.set("views", process.cwd() + '/views');
 
 app.listen(5000, (req,res) => {
     console.log("Server on");
